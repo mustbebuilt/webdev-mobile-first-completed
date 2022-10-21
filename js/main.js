ff716682e3f200a6bcd435441078f0e8fc2dd865
mustbebuilt <martin@mustbebuilt.co.uk>
2022-10-21 17:02:01 +0100

    Refactored JS

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
 (function () {
-  var setMenu = function () {
+  let setMenu = function () {
     // variables
-    var burgerMenu = document.querySelector(".burger");
-    var navBar = document.querySelector("nav");
-    var navBarStatus = false;
+    let burgerMenu = document.querySelector(".burger");
+    let navBar = document.querySelector("nav");
+    let navBarStatus = false;
     navBar.removeAttribute("style");
-    // mobile menu
+    // mobile menu via CSS Animation
     if (window.getComputedStyle(navBar).overflow == "hidden") {
-      var navHeight = navBar.offsetHeight;
+      let navHeight = navBar.offsetHeight;
       navBar.style.height = "0px";
-      burgerMenu.addEventListener("click", function () {
+      burgerMenu.addEventListener("click", () => {
         if (navBarStatus) {
           navBarStatus = false;
           navBar.style.height = "0px";
-          //   burgerMenu.setAttribute("src", "images/whiteMenu.svg");
         } else {
           navBarStatus = true;
           navBar.style.height = navHeight + "px";
-          //   burgerMenu.setAttribute("src", "images/whiteClose.svg");
         }
       });
     } else {
       navBarStatus = false;
       navBar.removeAttribute("style");
-      //   burgerMenu.setAttribute("src", "images/whiteMenu.svg");
     }
   };
   setMenu();
   // resize
-  window.addEventListener("resize", function () {
-    setMenu();
-  });
+  window.addEventListener("resize", () => setMenu());
 })();
