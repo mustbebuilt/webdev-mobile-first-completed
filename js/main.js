afe822423774e9559b03ac994ffa9ea891cb8d75
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2022-10-23 13:25:25 +0100

    Animated Menu

--- js/main.js
+++ js/main.js
@@ -10,12 +10,17 @@
       let navHeight = navBar.offsetHeight;
       navBar.style.height = "0px";
       burgerMenu.addEventListener("click", () => {
+        console.info(burgerMenu.classList);
         if (navBarStatus) {
           navBarStatus = false;
           navBar.style.height = "0px";
+          // burger animation class
+          burgerMenu.classList.toggle("changeBurger");
         } else {
           navBarStatus = true;
           navBar.style.height = navHeight + "px";
+          // burger animation class
+          burgerMenu.classList.toggle("changeBurger");
         }
       });
     } else {
